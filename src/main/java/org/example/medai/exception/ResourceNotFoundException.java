81b685e3c879dda7fa2d6eed6baee6979d1577d0
“zuunun” <zuun0001@stud.kea.dk>
2025-04-05 13:15:12 +0200

    Added java doc + ErrorMessage

---
+++ src/main/java/org/example/medai/exception/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package org.example.medai.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
