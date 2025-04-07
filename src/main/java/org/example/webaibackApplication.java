83fc27025b09c1b24783fa2950531a4f6735534c
“zuunun” <zuun0001@stud.kea.dk>
2025-04-07 15:18:52 +0200

    webaibackApplication

--- src/main/java/org/example/webaibackApplication.java
+++ src/main/java/org/example/webaibackApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MedAiApplication {
+public class webaibackApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MedAiApplication.class, args);
+        SpringApplication.run(webaibackApplication.class, args);
     }
 
 }
