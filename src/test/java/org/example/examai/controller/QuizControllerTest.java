c57494a9946d440fa03924e5ce6189d1493f8fad
amaliekea <amje0001@stud.kea.dk>
2025-04-20 20:33:38 +0200

    codecomments

--- src/test/java/org/example/examai/controller/QuizControllerTest.java
+++ src/test/java/org/example/examai/controller/QuizControllerTest.java
@@ -4,8 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.examai.dto.StudyQuestion;
 import org.example.examai.service.QuizService;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
