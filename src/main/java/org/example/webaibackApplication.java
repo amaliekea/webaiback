474657df04926e1ae48c8035c36b21afc1ee044b
amaliekea <amje0001@stud.kea.dk>
2025-04-23 12:31:45 +0200

    added swagger

--- src/main/java/org/example/webaibackApplication.java
+++ src/main/java/org/example/webaibackApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class webaibackApplication {
+    //YAML api-dokumentation hentes her: http://localhost:8080/v3/api-docs.yaml
+    //JSON api-dokumentation hentes her: http://localhost:8080/v3/api-docs
+    //swagger: http://localhost:8080/swagger-ui.html
 
     public static void main(String[] args) {
         SpringApplication.run(webaibackApplication.class, args);
